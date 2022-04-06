1aedae3de94174669651931a3d7e191f640f89b8
Brandon <robertsbt@gmail.com>
2022-04-06 11:18:36 -0500

    chore: add live endpoint

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,5 @@
 import { Appwrite } from 'appwrite';
 
 export const api = new Appwrite();
-api.setEndpoint('http://localhost/v1')
+api.setEndpoint('http://147.182.227.168/v1')
 api.setProject('chat');
